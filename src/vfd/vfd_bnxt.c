2c7ad8c4fdb7a2264dd2ff5cbdfef3735a0fbc6e
dave kormann <lossage@gmail.com>
2018-05-03 10:12:47 -0400

    Update vfd_bnxt.c

--- src/vfd/vfd_bnxt.c
+++ src/vfd/vfd_bnxt.c
@@ -376,6 +376,7 @@ vfd_bnxt_vf_msb_event_callback(uint16_t port_id, enum rte_eth_event_type type, v
 		case HWRM_CFA_TUNNEL_FILTER_FREE:
 		case HWRM_TUNNEL_DST_PORT_QUERY:
 			add_refresh = true;
+			break;
 		/* Allow */
 		case HWRM_VER_GET:
 		case HWRM_FUNC_DRV_RGTR:
